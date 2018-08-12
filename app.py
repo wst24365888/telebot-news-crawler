0322d419ca9d669a814f7b1e65f5eca4105de093
wst24365888 <hansen0715.2@gmail.com>
2018-08-12 22:17:32 +0800

    test

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://telegram-20180812.herokuapp.com/' + TOKEN)
+    bot.set_webhook(url='https://telebot-20180812.herokuapp.com/' + TOKEN)
     return "!", 200
 
 
