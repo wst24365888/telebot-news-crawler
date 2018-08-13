aa9680f3f65ba2b3c95204922254ed283c916cdc
wst24365888 <hansen0715.2@gmail.com>
2018-08-13 21:26:50 +0800

    test

--- database.py
+++ database.py
@@ -1,8 +1,9 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import os
 
-cred = credentials.Certificate('serviceAccount.json')
+cred = credentials.Certificate(os.environ['serviceAccount'])
 
 firebase_admin.initialize_app(cred)
 
