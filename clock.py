0c972ea79b94a03e0a85dd59af99b0a2ce081a6d
wst24365888 <hansen0715.2@gmail.com>
2018-08-13 22:30:14 +0800

    test

--- clock.py
+++ clock.py
@@ -2,7 +2,7 @@ import os
 from apscheduler.schedulers.blocking import BlockingScheduler
 sched = BlockingScheduler()
  
-@sched.scheduled_job('interval', minutes=1)
+@sched.scheduled_job('interval', seconds=5)
 def cr():
     print('Do crawler.')
     #os.system("python crawler.py")
