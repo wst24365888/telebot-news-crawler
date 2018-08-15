c4b8f31bf5a6dfcbc099a42702dbe9cdc1afad50
wst24365888 <hansen0715.2@gmail.com>
2018-08-15 09:17:19 +0800

    test

--- clock.py
+++ clock.py
@@ -1,10 +1,13 @@
 import os
 from apscheduler.schedulers.blocking import BlockingScheduler
 sched = BlockingScheduler()
+
+i = 1
  
 @sched.scheduled_job('interval', seconds=30)
 def cr():
-    print('Do crawler.')
+    print('{}th time doing crawler.'.format(i))
     os.system("python crawler.py")
+    i+=1
 
 sched.start()
